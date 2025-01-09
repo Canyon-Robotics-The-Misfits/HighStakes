1961037dfd4ad3adbf0ddc8fc5fb5998b46321ac
iseau395 <70857830+iseau395@users.noreply.github.com>
2025-01-09 15:40:18 -0800

    negative blue basically done

--- src/auto_routes/negative_blue.cpp
+++ src/auto_routes/negative_blue.cpp
@@ -13,6 +13,7 @@ AUTO_ROUTE(auto_routes::negative_blue)
     pros::delay(250);
     drive_controller->drive(9, { min_speed: 30 });
     arm->set_state(ArmState::LOAD);
+    drivetrain->move(0, 40);
     pros::delay(200);
 
     // grab goal
@@ -22,32 +23,48 @@ AUTO_ROUTE(auto_routes::negative_blue)
     intake->set_state(IntakeState::HOOD);
     
     // get line rings
-    drive_controller->drive_to(pose(144 - 23, 72 - 12 + 6, 90_deg), { max_speed: 110 });
-    pros::delay(100);
-    // drive_controller->drive_time(127, 200);
+    drive_controller->drive_to(pose(144 - 29, 72 - 12 + 6, 90_deg), { max_speed: 110 });
+    pros::delay(200);
+    drive_controller->drive_time(127, 300);
+    pros::delay(200);
     intake->set_state(IntakeState::DISABLED);
 
     // get stack
     drive_controller->boomerang(pos(144-48, 48), { backwards: true, threshold: 2, min_speed: 40 });
     intake->set_state(IntakeState::HOOD);
     drive_controller->boomerang(pos(144 - 24, 48), { threshold: 3 });
+    pros::delay(100);
     drive_controller->drive(-10, { min_speed: 60, chained: true });
 
-    // get corner
-    arm->set_state(ArmState::ALLIANCE_STAKE);
-    drive_controller->boomerang(pos(144-30, 30));
-    intake->set_state(IntakeState::HOOD);
-    drive_controller->face_angle(odometry->get_pose().vec().angle_to(lib15442c::Vec(144, 0)));
-    pros::delay(50);
-    drivetrain->move(127, 0);
-    pros::delay(700);
-    drivetrain->move(0, 0);
-    pros::delay(100);
-    drivetrain->move(127, 0);
-    pros::delay(100);
-    arm->set_state(ArmState::LADDER_TOUCH);
+    // // get corner
+    // arm->set_state(ArmState::ALLIANCE_STAKE);
+    // drive_controller->boomerang(pos(144-30, 30));
+    // intake->set_state(IntakeState::HOOD);
+    // drive_controller->face_angle(odometry->get_pose().vec().angle_to(lib15442c::Vec(144, 0)));
+    // pros::delay(50);
+    // drivetrain->move(127, 0);
+    // pros::delay(700);
+    // drivetrain->move(0, 0);
+    // pros::delay(100);
+    // drivetrain->move(127, 0);
+    // pros::delay(100);
+    // arm->set_state(ArmState::LADDER_TOUCH);
 
     // get center ring
-    drive_controller->boomerang(pos(144 - 48, 24));
-    drive_controller->face_angle(-90_deg);
+    drive_controller->boomerang(pos(144 - 48, 24+6.5));
+    drive_controller->face_angle(-90_deg, { chained: true });
+    drive_controller->face_angle(-90_deg, { max_speed: 40 });
+    arm->set_state(ArmState::LADDER_TOUCH);
+    intake->set_state(IntakeState::WALL_STAKE);
+    drive_controller->drive(26);
+    pros::delay(300);
+    intake->set_state(IntakeState::HOOD);
+    drive_controller->drive(14, { max_speed: 60, min_speed: 40, chained: true });
+    pros::delay(200);
+
+    // touch ladder
+    drive_controller->boomerang(pos(144-48, 48), { backwards: true, threshold: 2, min_speed: 60 } );
+    drive_controller->face_angle(-45_deg, { threshold: 8_deg });
+    drive_controller->drive_time(100, 500);
+    drivetrain->set_brake_mode(lib15442c::MotorBrakeMode::COAST);
 }
