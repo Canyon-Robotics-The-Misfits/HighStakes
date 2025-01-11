a467ececc562cf751e7d20aa05f89b84ac37c37e
iseau395 <70857830+iseau395@users.noreply.github.com>
2025-01-10 16:14:13 -0800

    remove corner from positive blue

--- src/auto_routes/positive_blue.cpp
+++ src/auto_routes/positive_blue.cpp
@@ -12,11 +12,16 @@ AUTO_ROUTE(auto_routes::positive_blue)
     doinker.extend();
     intake->set_state(IntakeState::HOOD);
     // drive_controller->drive_to(pose(144-23, 53.5, -25_deg), { threshold: 1 });
-    drive_controller->drive(37.5, { angle: odometry->get_rotation() - 5_deg, threshold: 1 });
+    auto goal_rush = drive_controller->drive(37.5, { angle: odometry->get_rotation() - 5_deg, threshold: 1, async: true });
+    arm->move_manual(127);
+    pros::delay(200);
+    arm->move_manual(0);
+    goal_rush->await();
     doinker.retract();
     pros::delay(50);
-    drive_controller->drive_time(-127, 250);
     intake->set_state(IntakeState::DISABLED);
+    arm->set_state(ArmState::LOAD);
+    drive_controller->drive_time(-127, 250);
     drive_controller->drive_time(-80, 200);
     doinker.extend();
     pros::delay(100);
@@ -32,7 +37,7 @@ AUTO_ROUTE(auto_routes::positive_blue)
     pros::delay(100);
 
     // score alliance stake
-    auto drive_to_alliance_stake = drive_controller->drive_to(pose(72-9-8 + 2, 16.5 - 2, 147_deg), { min_speed: 25, async: true });
+    auto drive_to_alliance_stake = drive_controller->drive_to(pose(72-9-8 + 2+5-3, 16.5 - 2, 147_deg), { min_speed: 25, async: true });
     arm->set_state(ArmState::ALLIANCE_STAKE);
     intake->set_state(IntakeState::REVERSE);
     pros::delay(100);
@@ -56,28 +61,28 @@ AUTO_ROUTE(auto_routes::positive_blue)
     // get rush goal
     drive_controller->drive(15, {min_speed: 60, chained: true });
     drive_controller->face_angle(-60_deg + 180_deg, {threshold: 3_deg, chained: true });
-    drive_controller->boomerang(pos(20, 65-10), { backwards: true, threshold: 6, min_speed: 60 });
+    drive_controller->boomerang(pos(20, 65-10-4), { backwards: true, threshold: 6, min_speed: 60 });
     clamp.extend();
 
     // get corner 
-    arm->set_state(ArmState::ALLIANCE_STAKE);
-    intake->set_state(IntakeState::REVERSE);
-    drive_controller->boomerang(pose(30-12, 30, 180_deg), { lead: 0.4, threshold: 1.5, min_speed: 30 } );
-    drive_controller->drive_time(-100, 150);
-    intake->set_state(IntakeState::HOOD);
-    drive_controller->face_angle(odometry->get_pose().vec().angle_to(lib15442c::Vec(0, 0)));
-    pros::delay(50);
-    drivetrain->move(127, 0);
-    pros::delay(700);
-    drivetrain->move(0, 0);
-    pros::delay(100);
-    drivetrain->move(127, 0);
-    pros::delay(100);
-    arm->set_state(ArmState::LADDER_TOUCH);
+    // arm->set_state(ArmState::ALLIANCE_STAKE);
+    // intake->set_state(IntakeState::REVERSE);
+    // drive_controller->boomerang(pose(30-12+3, 30-6, 180_deg), { lead: 0.4, threshold: 1.5, min_speed: 30 } );
+    // drive_controller->drive_time(-100, 150);
+    // intake->set_state(IntakeState::HOOD);
+    // drive_controller->face_angle(odometry->get_pose().vec().angle_to(lib15442c::Vec(0, 0)));
+    // pros::delay(50);
+    // drivetrain->move(127, 0);
+    // pros::delay(700);
+    // drivetrain->move(0, 0);
+    // pros::delay(100);
+    // drivetrain->move(127, 0);
+    // pros::delay(100);
 
     // touch ladder
-    drive_controller->boomerang(pos(40, 36), { backwards: true, threshold: 2, min_speed: 60 } );
+    arm->set_state(ArmState::LADDER_TOUCH);
+    drive_controller->boomerang(pos(40, 36 +12), { threshold: 2, min_speed: 60 } );
     drive_controller->face_angle(45_deg, { threshold: 8_deg });
-    drive_controller->drive_time(80, 500);
+    drive_controller->drive_time(80, 200);
     drivetrain->set_brake_mode(lib15442c::MotorBrakeMode::COAST);
 }
