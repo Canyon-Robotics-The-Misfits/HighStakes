71829f84d3638d60df77927044932dec89d1ce71
iseau395 <70857830+iseau395@users.noreply.github.com>
2025-02-04 09:40:07 -0800

    tweak skills test after speeding up intake

--- src/auto_routes/test.cpp
+++ src/auto_routes/test.cpp
@@ -22,16 +22,16 @@ AUTO_ROUTE(auto_routes::skills_triple_test)
     intake->set_state(IntakeState::HOOD);
     drive_controller->boomerang(pos(144 - 48, 48), { threshold: 3, min_speed: 60 });
     drive_controller->drive_time(60, 100);
-    drive_controller->boomerang(pos(144 - 14, 72 +2));
+    drive_controller->boomerang(pos(144 - 12, 72 +2));
     drive_controller->drive(-4, { min_speed: 80, chained: true });
     
     // get rings near corner; dropoff goal
-    drive_controller->face_point(lib15442c::Vec(144 - 24, 72 - 24), 0_deg, { threshold: 15_deg, chained: true });
-    drive_controller->boomerang(pos(144 - 24, 72 - 24), { threshold: 2 });
-    drive_controller->boomerang(pos(144 - 24, 24 + 1), { min_speed: 25 });
-    pros::delay(50);
-    drive_controller->boomerang(pos(144 - 24, 12 - 1), { min_speed: 25 });
-    pros::delay(50);
+    drive_controller->face_point(lib15442c::Vec(144 - 24, 72 - 24), 0_deg, { threshold: 5_deg, chained: true });
+    drive_controller->boomerang(pos(144 - 24, 72 - 24), { threshold: 5, min_speed: 110 });
+    drive_controller->boomerang(pos(144 - 24, 24), { threshold: 5, min_speed: 60 });
+    drive_controller->boomerang(pos(144 - 24, 12 + 1), { min_speed: 25 });
+    pros::delay(150);
+    // odometry->set_x(142 - distance_left()); // reset odom w/ distance sensors
     drive_controller->face_angle(45_deg, { threshold: 10_deg, chained: true });
     drive_controller->boomerang(pos(144 - 12 -1, 24), { min_speed: 50 });
     drive_controller->boomerang(pos(144 - 18, 18), { backwards: true, threshold: 5, min_speed: 80 });
