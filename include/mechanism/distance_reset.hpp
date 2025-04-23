78fd3f3ca5598f7b37d67d537ee453150ee5cefc
iseau395 <70857830+iseau395@users.noreply.github.com>
2025-04-23 11:36:20 -0700

    add more conditions to distance resets

--- include/mechanism/distance_reset.hpp
+++ include/mechanism/distance_reset.hpp
@@ -10,12 +10,12 @@ namespace mechanism
 {
     using namespace lib15442c::literals;
 
-    constexpr double RESET_THRESHOLD_DEG = 5; 
+    constexpr double RESET_THRESHOLD_DEG = 8; 
 
     constexpr double MM_TO_IN = 0.0393701;
-    constexpr double LEFT_DISTANCE_OFFSET = 0;
-    constexpr double RIGHT_DISTANCE_OFFSET = 0;
-    constexpr double FRONT_DISTANCE_OFFSET = 0;
+    constexpr double LEFT_DISTANCE_OFFSET = 6;
+    constexpr double RIGHT_DISTANCE_OFFSET = 6;
+    constexpr double FRONT_DISTANCE_OFFSET = 2.625;
 
-    void distance_reset(std::shared_ptr<lib15442c::TrackerOdom> odometry);
+    void distance_reset(std::shared_ptr<lib15442c::TrackerOdom> odometry, bool disable_left = false, bool disable_right = false, bool disable_front = false);
 } // namespace mechanism
--- src/mechanism/distance_reset.cpp
+++ src/mechanism/distance_reset.cpp
@@ -2,92 +2,117 @@
 
 #define LOGGER "distance_reset.cpp"
 
-void mechanism::distance_reset(std::shared_ptr<lib15442c::TrackerOdom> odometry)
+bool check_conditions(pros::Distance sensor, std::string name)
 {
+    if (!sensor.is_installed())
+    {
+        ERROR("%s distance sensor not detedted on port %d!", name.c_str(), sensor.get_port());
+        return false;
+    }
+    if (sensor.get() == 9999)
+    {
+        WARN("%s distance sensor is out of range!", name.c_str());
+        return false;
+    }
+    if (sensor.get() * mechanism::MM_TO_IN * 0.05 > 2)
+    {
+        WARN("%s distance sensor has an error of >2!", name.c_str());
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void mechanism::distance_reset(std::shared_ptr<lib15442c::TrackerOdom> odometry, bool disable_left, bool disable_right, bool disable_front)
+{
+    lib15442c::Vec initial_pos = odometry->get_position();
+
     pros::Distance left_sensor = pros::Distance(config::PORT_DISTANCE_LEFT);
     pros::Distance right_sensor = pros::Distance(config::PORT_DISTANCE_RIGHT);
     pros::Distance front_sensor = pros::Distance(config::PORT_DISTANCE_FRONT);
+
+    bool left_passed = !disable_left && check_conditions(left_sensor, "left");
+    bool right_passed = !disable_right && check_conditions(right_sensor, "right");
+    bool front_passed = !disable_front && check_conditions(front_sensor, "front");
 
     double left = left_sensor.get() * MM_TO_IN + LEFT_DISTANCE_OFFSET;
     double right = right_sensor.get() * MM_TO_IN + RIGHT_DISTANCE_OFFSET;
     double front = front_sensor.get() * MM_TO_IN + FRONT_DISTANCE_OFFSET;
 
-    bool left_in_range = left_sensor.get() != 9999 && left_sensor.is_installed();
-    bool right_in_range = left_sensor.get() != 9999 && right_sensor.is_installed();
-    bool front_in_range = left_sensor.get() != 9999 && front_sensor.is_installed();
-
     lib15442c::Angle heading = odometry->get_rotation();
 
-    if (!(left_in_range || right_in_range))
+    if (!(left_passed || right_passed))
     {
         WARN_TEXT("No horrizontal sensor in range for reset!");
     }
     
-    if (!front_in_range)
+    if (!front_passed)
     {
         WARN_TEXT("Front sensor out of range for reset!");
     }
 
     if (abs(heading.deg()) < RESET_THRESHOLD_DEG)
     {
-        if (front_in_range)
+        if (front_passed)
         {
             odometry->set_y(144 - front);
         }
 
-        if (left < right && left_in_range)
+        if (left < right && left_passed)
         {
             odometry->set_x(left);
         }
-        else if (right_in_range)
+        else if (right_passed)
         {
             odometry->set_x(144 - right);
         }
     }
     else if (abs((heading - 90_deg).deg()) < RESET_THRESHOLD_DEG)
     {
-        if (left < right && left_in_range)
+        if (left < right && left_passed)
         {
             odometry->set_y(144 - left);
         }
-        else if (right_in_range)
+        else if (right_passed)
         {
             odometry->set_y(right);
         }
         
-        if (front_in_range)
+        if (front_passed)
         {
             odometry->set_x(144 - front);
         }
     }
     else if (abs((heading - 180_deg).deg()) < RESET_THRESHOLD_DEG)
     {
-        if (front_in_range)
+        if (front_passed)
         {
             odometry->set_y(front);
         }
 
-        if (left < right && left_in_range)
+        if (left < right && left_passed)
         {
-            odometry->set_x(left);
+            odometry->set_x(144 - left);
         }
-        else if (right_in_range)
+        else if (right_passed)
         {
-            odometry->set_x(144 - right);
+            odometry->set_x(right);
         }
     }
     else if (abs((heading - 270_deg).deg()) < RESET_THRESHOLD_DEG)
     {
-        if (left < right && left_in_range)
+        if (left < right && left_passed)
         {
             odometry->set_y(left);
         }
-        else if (right_in_range)
+        else if (right_passed)
         {
             odometry->set_y(144 - right);
         }
         
-        if (front_in_range)
+        if (front_passed)
         {
             odometry->set_x(front);
         }
@@ -96,4 +121,20 @@ void mechanism::distance_reset(std::shared_ptr<lib15442c::TrackerOdom> odometry)
     {
         WARN_TEXT("Heading not within range for reset!");
     }
+    
+    lib15442c::Vec end_pos = odometry->get_position();
+
+    if (abs(initial_pos.x - end_pos.x) > 7)
+    {
+        WARN_TEXT("Distance reset >7 inches off on x from odom position, ignoring x reset!");
+        end_pos.x = initial_pos.x;
+    }
+
+    if (abs(initial_pos.y - end_pos.y) > 7)
+    {
+        WARN_TEXT("Distance reset >7 inches off on y from odom position, ignoring y reset!");
+        end_pos.y = initial_pos.y;
+    }
+
+    printf("(%f, %f) changed to (%f, %f)\n", initial_pos.x, initial_pos.y, end_pos.x, end_pos.y);
 }
